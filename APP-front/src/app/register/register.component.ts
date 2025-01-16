15a00011d9e8a53772e654c3055d468330739cec
Yasser <khalfalliyasser27@gmail.com>
2025-01-16 21:01:10 +0100

    create the App's api

--- APP-front/src/app/register/register.component.ts
+++ APP-front/src/app/register/register.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  form = {
+    surname: null,
+    name: null,
+    email: null,
+    password: null,
+    confirmPassword: null,
+    userRole: 'freelance'
+  };
+  constructor() { }
+  ngOnInit() { }
 
 }
